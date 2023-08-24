675a19ab1310bf510a2515b5fd4a7e810f7543ca
Tausia01 <tausia.tahsin@gmail.com>
2023-08-24 21:18:05 +0600

    added Back button for Customer registration

--- src/Administratorpkg/CreateNewAccountCustomerSceneController.java
+++ src/Administratorpkg/CreateNewAccountCustomerSceneController.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.time.LocalDate;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -88,13 +89,24 @@ public class CreateNewAccountCustomerSceneController implements Initializable {
 
     }
 
-    @FXML
     private void BackbuttonOnClick(MouseEvent event) throws IOException {
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         Parent root = FXMLLoader.load(getClass().getResource("/mainpkg/SelectUserScene.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+
+    @FXML
+    private void BackbuttonOnClick(ActionEvent event) throws IOException {
+        
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("/customerpkg/CustomerLoginPage.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        
+        
     }
     
 }
